205a397649d7ff7617d7f423a8a25f4aeb8a77a3
Ethan Glenn <ethan+git@eglenn.dev>
2024-03-05 18:31:46 -0700

    Updated with node-fetch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Mustache = require('mustache');
 const fs = require('fs');
+const fetch = require('node-fetch');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
 const username = 'eglenn-dev';
